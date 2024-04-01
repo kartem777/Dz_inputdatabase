a65c54985013e24c02a641c27bfcb38c69db5341
Artem K <artiukraineyt@gmail.com>
2024-04-01 14:25:27 +0200

    Add project files.

---
+++ qwedq/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace qwedq
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+        public string connectionString;
+        public ApplicationContext() : base()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=test.db");
+        }
+    }
+}
---
+++ qwedq/Program.cs
@@ -0,0 +1,39 @@
+﻿using qwedq;
+
+bool flag = false;
+while (flag != true)
+{
+    using (ApplicationContext db = new ApplicationContext())
+    {
+        try
+        {
+            Console.WriteLine("Name:");
+            string? name = Console.ReadLine();
+            Console.WriteLine("Age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Is Premium:");
+            bool isP = Convert.ToBoolean(Console.ReadLine());
+            User user = new User { Name = name, Age = age, IsPremium = isP };
+            db.Users.Add(user);
+            db.SaveChanges();
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine("Pls try again");
+        }
+        var users = db.Users.ToList();
+        Console.WriteLine("List:");
+        foreach (User u in users)
+            Console.WriteLine($"{u.Id}. {u.Name} -> {u.Age} -> {u.IsPremium}");
+        Console.WriteLine("Exit?");
+        try
+        {
+            flag = Convert.ToBoolean(Console.ReadLine());
+        }
+        catch( Exception ex )
+        {
+            flag = true;
+        }
+    }
+}
